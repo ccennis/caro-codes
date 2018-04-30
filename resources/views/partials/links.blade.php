4577f7a01e7758d2fa63c798e88d788f0331cc43
Caroline Ennis <caroline.ennis@healthcareassetnetwork.com>
2018-04-30 13:38:11 -0400

    adding link to forman group page

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -1,7 +1,16 @@
 <style>
 
-    .icon {
+    .icon-reidbuilds {
         background: url('/img/house-icon.png');
+        background-position: center center;
+        background-repeat: no-repeat;
+        height: 100px;
+        width: 100px;
+        display: block;
+    }
+
+    .icon-formangroup {
+        background: url('/img/key-icon.png');
         background-position: center center;
         background-repeat: no-repeat;
         height: 100px;
@@ -13,7 +22,8 @@
 
 <div class="container">
     <div class="col-lg-8 col-xs-12 col-lg-offset-2" id="links" style="text-align:center; display:none">
-        <a href="http://www.reidbuilds.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon fa-5x"></span><br>Reid Builds</a>
+        <a href="http://www.reidbuilds.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon-reidbuilds fa-5x"></span><br>Reid Builds</a>
+        <a href="http://www.formanrealtygroup.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon-formangroup fa-5x"></span><br>The Forman Group</a>
     </div>
 </div>
 
