7b61b6c2a3f28b81aa8f33ba8f371149996bd640
adding gitignore <>
2020-02-28 16:49:42 -0800

    removing all these worthless packages

--- resources/views/partials/links.blade.php
+++ resources/views/partials/links.blade.php
@@ -23,7 +23,7 @@
 <div class="container">
     <div class="col-lg-8 col-xs-12 col-lg-offset-2" id="links" style="text-align:center; display:none">
         <a href="http://www.reidbuilds.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon-reidbuilds fa-5x"></span><br>Reid Builds</a>
-        <a href="http://www.formanrealtygroup.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon-formangroup fa-5x"></span><br>The Forman Group</a>
+        <a href="https://www.findinglouisvillehomes.com" class="btn btn-lg text-center" target="_blank" style="color:black"><span class="icon-formangroup fa-5x"></span><br>Forman, Jones & Associates Realtors</a>
     </div>
 </div>
 
